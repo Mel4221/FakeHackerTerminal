411773b1a21120b11a3a6c963e189a877ac9c446
Melquiceded Balbi Villanueva <melqui4251@gmail.com>
2022-09-20 01:01:54 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using QuickTools;
+
+namespace FakeHackerScreen
+{
+    class MainClass
+    {
+        static int X = Console.BufferWidth;
+        static int Y = Console.BufferHeight;
+        static int Cx = New.RandomInt(1, X - 1);
+        static int Cy = New.RandomInt(1, Y - 1);
+        static int Interval = 100; 
+
+       // static bool R = true; 
+        public static void Main(string[] args)
+        {
+            Console.Title = "Hacking...";
+            Get.Clear();
+            Func<string> type = () => 
+            {
+                string text = null; 
+                if(args.Length > 0 )
+                {
+                    switch(args[0])
+                    {
+                        case "n":
+                            text = New.Pin(X*Y); 
+                        break;
+                        case "-n":
+                            text = New.Pin(int.Parse(args[1])); 
+                            break;
+                        case "t":
+                            Interval = int.Parse(args[1]); 
+                            break; 
+                    }
+
+                    return text;
+                }
+                else
+                {
+                    return New.Password(X * Y);
+                }
+
+
+            };
+
+            while (true)
+            {
+                Console.SetCursorPosition(Cx, Cy);
+                string randomText = type(); //New.Pin(X*Y+100).ToString();//
+                Color.Green(randomText);
+                Get.WaitTime(Interval);
+            }
+
+
+        }
+    }
+}
